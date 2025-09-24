e369b65925abe043672e7993247219fb916e7e8d
ryan theerman <ryantheerman@proton.me>
2025-09-23 20:44:36 -0400

    init

---
+++ chapter-1/notes.rkt
@@ -0,0 +1,14 @@
+; this is a comment 
+(inc 55)
+
+; with tslime installed and configured in vim, i can press <C-h><C-t> on a given line, and that line will be sent to my racket interpreter in the next pane over
+(inc 2001)
+
+; if there is not a blank space between the previous line and the currently selected line, my tslime shortcuts will send all lines above the current until a line break. this includes comments and other lisp commands
+; it will also send lines below the current
+; so unless the line i want to sound is bounded top and bottom by blank lines, they'll get sent via tslime too.
+; not the end of the world.
+
+(inc 20034)
+
+(inc 201)
